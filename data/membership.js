2dc394135b85c44c75b0b2595d584bdb9986ea41
alex-halloran <alex+github@alexhalloran.com>
2025-03-11 19:58:51 -0700

    Add membership information

---
+++ data/membership.js
@@ -0,0 +1,11 @@
+// Information about membership options
+
+module.exports = {
+  info: "Pickleball Kingdom offers various membership options to fit your playing needs. Please visit our website or contact our facility for detailed information on current membership rates and benefits.",
+  
+  benefits: "Membership benefits include priority court booking, discounted court rates, special member events, and more.",
+  
+  cancellation: "For monthly membership payments, you must notify us of your cancellation or pausing request no less than 30 days before the next recurring billing date to avoid another payment. No refunds of any partial month payments will be given upon the notice of cancellation of monthly recurring membership payments. This cancellation notice needs to be given in person at the club. If a member is injured or out of state, then an email needs to be sent to the manager of the club with a copy of the member's drivers license or state ID.\n\nFor annual membership payments, you must notify us of your cancellation request within 7 days after the date on which you signed up for your membership in order to obtain a full refund of the annual payment amount. After 7 days have expired, no refund will be given for any amount of the annual payment if you cancel your membership.",
+  
+  pausing: "Any membership account, except for annual plans, can be paused once per 12 months. Member needs to contact the Operator's manager in writing to have their membership paused or restarted. There is an administrative fee to pause the membership and a recurring monthly charge while membership is paused. The pausing date needs to be no less than 72 hours before the billing date or the funds will be pulled.\n\nWhen restarting a monthly membership after pausing it, the Member is subject to the current price of the membership and not what it was when the membership was paused. If the club's capacity membership number has been hit, then the membership will be placed on the waiting list until a spot opens up for it to be reinstated into. There is no guarantee of when that reinstatement will be."
+};
